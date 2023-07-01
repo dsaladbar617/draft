c95b7935474af018387a96f994c149471b99344f
dsalazar219 <dsalazar219@yahoo.com>
2023-07-01 17:14:47 -0500

    Revert "Revert "routing broken""
    This reverts commit 285cd650edfb7a7c4a0d0a9020e26a016f88f906.

--- components/DraftTable.tsx
+++ components/DraftTable.tsx
@@ -26,30 +26,30 @@ const DraftTable = ({ data, selectedTeam }: TableProps) => {
 	if (selectedTeam === '0') draft = data?.drafts[0].rounds;
 
 	return (
-		<table className='table mx-auto mt-10 table-responsive rounded-md text-center p-4 w-1/2 border-collapse'>
-			<thead className='table-header-group bg-slate-500 rounded-lg p-4'>
-				<tr className='table-row rounded'>
+		<div className='table mx-auto mt-10 table-responsive rounded-md text-center p-4 w-2/3 border-collapse'>
+			<div className='table-header-group bg-slate-500 rounded-lg p-4'>
+				<div className='table-row rounded'>
 					{headers.map((header: string, index: number) => (
-						<th
+						<div
 							key={header}
 							className={`table-cell p-4 ${
 								index === headers.length - 1 ? 'rounded-tr-lg' : null
 							} ${index === 0 ? 'rounded-tl-lg' : null}`}>
 							{header}
-						</th>
+						</div>
 					))}
-				</tr>
-			</thead>
-			<tbody className='table-row-group divide-y divide-gray-300'>
+				</div>
+			</div>
+			<div className='table-row-group divide-y divide-gray-300'>
 				{draft?.map((round) => (
 					<Fragment key={uuidv4()}>
-						<tr className='table-row'>
-							<td
-								colSpan={3}
-								className='table-cell text-center font-bold text-xl p-4  col-span-3'>
+						<div className='table-row'>
+							<div className='table-cell text-center font-bold text-xl p-4 bg-slate-950 col-span-3'>
 								Round {round.round}
-							</td>
-						</tr>
+							</div>
+							<div className='col-span-3'> </div>
+							<div className='col-span-3'> </div>
+						</div>
 						{round.picks.map((pick) => (
 							<Link
 								href={{
@@ -57,7 +57,7 @@ const DraftTable = ({ data, selectedTeam }: TableProps) => {
 									query: { id: pick.prospect.id?.toString() }
 								}}
 								key={uuidv4()}
-								className='hover:bg-slate-400 hover:text-black table-row transition duration-200'>
+								className='hover:bg-slate-400 hover:text-black table-row'>
 								<div className='table-cell p-4'>{pick.pickOverall}</div>
 								<div className='table-cell p-4'>{pick.team.name}</div>
 								<div className='table-cell p-4'>{pick.prospect.fullName}</div>
@@ -65,8 +65,8 @@ const DraftTable = ({ data, selectedTeam }: TableProps) => {
 						))}
 					</Fragment>
 				))}
-			</tbody>
-		</table>
+			</div>
+		</div>
 	);
 };
 
