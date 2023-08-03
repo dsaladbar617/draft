b590702c4e7a411ebf2eebdbb9c30d616294a844
daniel <daniel@daniels-MacBook-Pro.local>
2023-08-03 13:39:29 -0500

    fixed import issues for vercel deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,3 @@
-'use client';
-
-import YearSelect from '@/components/YearSelect';
-import { useState } from 'react';
-import DraftTable from '../components/DraftTable';
-import TeamSelect from '@/components/TeamSelect';
-
 export default function Home() {
 	return (
 		<div>
